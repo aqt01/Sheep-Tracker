7da82a64dcc6635510e32044a16a57c69bbe727d
Lowell T. Abbott Vidal <lowell.abbott@gmail.com>
2016-05-23 08:48:59 +0000

    Add initial models WIP and fixed manage to use a local_settings

--- sheeptracker/sheeps/models.py
+++ sheeptracker/sheeps/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
 # Create your models here.
+
+class Cell(models.Model):
+    organization = ForeignKey(Organization)
+    address = OneToOneField(Address)
+
+class SocialMedia(models.Model):
+    name = models.CharField()
+    contact = ForeignKey(Contact)
+    
+class Address(models.Model):
+    
+
+# this is meta
+class Contact(models.Model):
+    name = models.CharField(max_length=80)
+    last_name = models.CharField(max_length=80)
+    email = models.EmailField()
+    # Should be replaced with number fields
+    cellphone = models.CharField(max_length=10)
+    telephone = models.CharField(max_length=10)
+    pic = models.FileField()
+
+
+class Member(Contact):
+    organization = ForeignKey(Organization)
+    
+    
